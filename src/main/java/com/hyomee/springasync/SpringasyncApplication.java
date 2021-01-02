f587d54443a30c31f343ef809e2a014e8297961e
hyosang <hyomee@naver.com>
2021-01-02 17:59:15 +0900

    최초 커밋

---
+++ src/main/java/com/hyomee/springasync/SpringasyncApplication.java
@@ -0,0 +1,13 @@
+package com.hyomee.springasync;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringasyncApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(SpringasyncApplication.class, args);
+  }
+
+}
---
+++ src/test/java/com/hyomee/springasync/SpringasyncApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hyomee.springasync;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringasyncApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
